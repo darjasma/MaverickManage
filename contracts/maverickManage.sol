b3cb4bf4266c1ab10f8a32afd00290060ffdba35
darjasma <darjasma@gmail.com>
2024-02-07 04:51:29 +0330

    sendUtil, receiveUtil, swap added and maverickManage upgraded

--- contracts/maverickManage.sol
+++ contracts/maverickManage.sol
@@ -30,7 +30,32 @@ contract maverickManage is IERC721Receiver {
 
     receive() external payable {}
 
-    function deposit(uint duration, bool doDelegation, bytes calldata _swapData) external {
+    //@Notice: increasing util token amount in the contract
+    //@Param _amount: the amount sender wants to add to the contract
+    function sendUtil(uint _amount) external{
+        utilToken.transferFrom(msg.sender, address(this), _amount); //TODO: Check if the sender should be msg.sender
+    }
+
+    //@Notice: Decreasing util token amount in the contract
+    //@Param: _amount: The amount to send to the _receiver
+    //@Param: _receiver: The address that receives the specified _amount of util token
+    function receiveUtil(uint _amount, address _receiver) external{
+        utilToken.transfer(_receiver, _amount);
+    }
+
+    function swap(bool[] calldata sendsETH, address[] calldata sendingToken, bytes[] calldata _swapsData) external returns(uint[] memory){
+        require(
+            sendsETH.length == _swapsData.length && _swapsData.length==sendingToken.length,
+            "sendsETH, _swapsData, sendingTokens arrays, all should have the same length"
+        );
+        uint [] memory receivedAmounts = new uint[](sendsETH.length);
+        for(uint i=0; i<sendsETH.length; i++){
+            receivedAmounts[i] = SwapHelper.swapLifi(sendsETH[i], sendingToken[i], _swapsData[i]);
+        }
+        return receivedAmounts;
+    }
+
+    function deposit(uint duration, bool doDelegation, bytes calldata _swapData) external { //TODO: swapdata removal
         uint receivedMAV = SwapHelper.swapLifi(false, Addresses.wEthTokenAddress, _swapData);
         IERC20(Addresses.MAVTokenAddress).approve(Addresses.veMAVTokenAddress, receivedMAV);
         IveMAV(Addresses.veMAVTokenAddress).stake(receivedMAV, duration, doDelegation);
@@ -41,82 +66,73 @@ contract maverickManage is IERC721Receiver {
         SwapHelper.swapLifi(false, Addresses.MAVTokenAddress, _swapData);
     }
 
-    //@Notice: If the pool belongs to eth-erc20 pass the swapData for swapping eth by _swapDataA
+    //@dev: If one side of the pool is eth the address of the token would be WETH token address.
     function addLiquidity(
-        bool ethPoolIncluded,
         IMaverickPool pool,
         uint256 tokenId,
         IMaverickPool.AddLiquidityParams[] calldata params,
         uint256 minTokenAAmount,
         uint256 minTokenBAmount,
-        uint256 deadline,
-        bytes calldata _swapDataA,
-        bytes calldata _swapDataB
+        uint256 deadline
     ) external payable returns (
         uint256 receivingTokenId,
         uint256 tokenAAmount,
         uint256 tokenBAmount,
         IMaverickPool.BinDelta[] memory binDeltas
     ){
-        uint receivedTokenAAmount = SwapHelper.swapLifi(false, address(utilToken), _swapDataA);
-        uint receivedTokenBAmount = SwapHelper.swapLifi(false, address(utilToken), _swapDataB);
-        uint sendEthAmount = ethPoolIncluded ? receivedTokenAAmount : 0;
         uint tokenARequiredAllowance;
         uint tokenBRequiredAllowance;
         for (uint i = 0; i < params.length; i++) {
             tokenARequiredAllowance += params[i].deltaA;
             tokenBRequiredAllowance += params[i].deltaB;
         }
+        uint sendEthAmount = 0;
         IERC20 tokenA = pool.tokenA();
         IERC20 tokenB = pool.tokenB();
-        tokenA.approve(Addresses.maverickRouterAddress, receivedTokenAAmount);
-        tokenB.approve(Addresses.maverickRouterAddress, receivedTokenBAmount);
-        (receivingTokenId,
+        tokenA.approve(Addresses.maverickRouterAddress, tokenARequiredAllowance);
+        tokenB.approve(Addresses.maverickRouterAddress, tokenBRequiredAllowance);
+//        console.log(address(tokenA));
+//        console.log(address(tokenB));
+//        console.log(tokenA.allowance(address(this), Addresses.maverickRouterAddress));
+//        console.log(tokenB.allowance(address(this), Addresses.maverickRouterAddress));
+        (
+            receivingTokenId,
             tokenAAmount,
             tokenBAmount,
-            binDeltas) = IMaverickRouter(Addresses.maverickRouterAddress).addLiquidityToPool{value: sendEthAmount}(
+            binDeltas
+        ) = IMaverickRouter(Addresses.maverickRouterAddress).addLiquidityToPool{value: sendEthAmount}(
             pool, tokenId, params, minTokenAAmount, minTokenBAmount, deadline
         );
         emit AddLiquidity(receivingTokenId, tokenAAmount, tokenBAmount, binDeltas);
+//        console.log(tokenA.allowance(address(this), Addresses.maverickRouterAddress));
+//        console.log(tokenB.allowance(address(this), Addresses.maverickRouterAddress));
     }
 
-    //@param: eth pool index : 0(tokenA), 1(tokenB), other(there is no eth in the pool)
+    //@dev: If the pool includes eth, the WETH token would be used instead
     function removeLiquidity(
-        uint ethPoolIndex,
         IMaverickPool pool,
         address recipient,
         uint256 tokenId,
         IMaverickPool.RemoveLiquidityParams[] calldata params,
         uint256 minTokenAAmount,
         uint256 minTokenBAmount,
-        uint256 deadline,
-        bytes calldata _swapDataA,
-        bytes calldata _swapDataB
-    ) external returns (uint256 tokenAAmount, uint256 tokenBAmount, IMaverickPool.BinDelta[] memory binDeltas, uint wethAmount){
+        uint256 deadline
+    ) external returns (uint256 tokenAAmount, uint256 tokenBAmount, IMaverickPool.BinDelta[] memory binDeltas){
         IMaverickPosition position = IMaverickRouter(Addresses.maverickRouterAddress).position();
         position.approve(Addresses.maverickRouterAddress, tokenId);
         (tokenAAmount, tokenBAmount, binDeltas) = IMaverickRouter(Addresses.maverickRouterAddress).removeLiquidity(
             pool, recipient, tokenId, params, minTokenAAmount, minTokenBAmount, deadline
         );
-        IERC20 tokenA = pool.tokenA();
-        IERC20 tokenB = pool.tokenB();
-        if (ethPoolIndex != 0) wethAmount = SwapHelper.swapLifi(false, address(tokenA), _swapDataA);
-        if (ethPoolIndex != 1) wethAmount += SwapHelper.swapLifi(false, address(tokenB), _swapDataB);
     }
 
-    function claimBoostedPositionRewards(IMaverickReward rewardContract,
-        bytes[] calldata _swapDatas, bool[] calldata swapIncludesETH, address[] calldata rewardTokens) external {
+    function claimBoostedPositionRewards(IMaverickReward rewardContract) external {
         IMaverickReward.EarnedInfo[] memory earnedInfo = rewardContract.earned(address(this));
         uint8 tokenIndex;
         for (uint i = 0; i < earnedInfo.length; i++) {
             if (earnedInfo[i].earned != 0) {
                 tokenIndex = rewardContract.tokenIndex(address(earnedInfo[i].rewardToken));
+                rewardContract.getReward(address(this), tokenIndex);
             }
-        }
-        require(_swapDatas.length==swapIncludesETH.length && swapIncludesETH.length==rewardTokens.length,
-            "All the swapIncludesETH, _swapDatas and rewardTokens array should have the same length");
-        for(uint i = 0; i<_swapDatas.length; i++){
-            SwapHelper.swapLifi(swapIncludesETH[i], rewardTokens[i], _swapDatas[i]);
         }
     }
 }
